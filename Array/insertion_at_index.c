0d9447b9ffe205f9817e27364b1097be543d0bea
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-04-30 10:04:28 +0530

    Update insertion_at_index.c

--- Array/insertion_at_index.c
+++ Array/insertion_at_index.c
@@ -1,22 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
+#define SIZE 10 
+
+void insertatend(int arr[],int *n,int index,int element)
+{
+    if(index<0 || index>*n)
+    {
+        printf("Invalid index");
+        return;
+    }
+
+    for(int i=*n;i>index;i--)
+        arr[i] = arr[i-1];
+    
+    arr[index] = element;
+    (*n)++;    
+}
 
 int main()
 {
-    int n,index,a;
-    int A[] = {10,20,30,40,50,60,70};
-    n = sizeof(A)/sizeof(A[0]);
+    int n,index,element;
+    int arr[SIZE] = {10,20,30,40,50,60};
+    n = 6;
 
     printf("Enter index number to enter element in array: ");
     scanf("%d",&index);
-
-    for(int i=n;i>index+1;i--)
-        A[i] = A[i-1];
-    
     printf("\nEnter element to add at index: ");
-    scanf("%d",&a);
-    A[index] = a;
+    scanf("%d",&element);
+
+    insertatend(arr,&n,index,element);
 
     printf("\nNew array: ");
     for(int i=0;i<n;i++)
-        printf("%d ",A[i]);
+        printf("%d ",arr[i]);
+    
+    return 0;
+
 }
