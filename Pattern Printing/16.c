cbf61fb2ddd0f6c609e446077ec161a02adcfb48
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-05-01 19:35:59 +0530

    Create 16.c

---
+++ Pattern Printing/16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d",&n);
+    char j = 'A';
+    for(int row=1;row<=n;row++)
+    {
+        for(int space=1;space<=n-row;space++)
+            printf(" ");
+
+        for(int col=1;col<=row;col++)
+            printf("%c",j);
+        j = j+1;
+        printf("\n");
+    }
+}
