9a7023ef86c33f74b62e838447db1eb51dd9fd6f
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-05-01 10:47:42 +0530

    Create 7.c

---
+++ Pattern Printing/7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    int static a=1;
+    printf("Enter the number of rows: ");
+    scanf("%d",&n);
+
+    for(int row=1;row<=n;row++)
+    {
+        for(int num=1;num<=row;num++)
+        {
+            printf("%d ",a);
+            a++;
+        }
+        printf("\n");
+    }
+}
