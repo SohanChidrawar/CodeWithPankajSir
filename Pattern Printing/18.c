c12ca9ac8ee70e28cc434dc8de3541564a31b369
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-05-01 19:36:40 +0530

    Create 18.c

---
+++ Pattern Printing/18.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d",&n);
+    char j='A';
+    for(int row=1;row<=n;row++)
+    {
+        for(int col=1;col<=row;col++)
+        {
+            printf("%c",j);
+            j++;
+        }
+        printf("\n");
+    }
+}
