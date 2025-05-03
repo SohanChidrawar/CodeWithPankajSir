4029972ce2034b98aa2e5a5c97fc45b0908b2246
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-05-03 12:56:39 +0530

    Create creating_a_LL.c

---
+++ LinkedList/creating_a_LL.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+struct Node* createnode(int key)
+{
+    struct Node* newnode = (struct Node*)malloc(sizeof(struct Node));
+    newnode->data = key;
+    newnode->next = NULL;
+    return newnode;
+    
+}
+
+int main()
+{
+    //create a LL 1->2->3->4->5->6->NULL
+    struct Node* START = createnode(1);
+    START->next = createnode(2);
+    START->next->next = createnode(3);
+    START->next->next->next = createnode(4);
+    START->next->next->next->next = createnode(5);
+    START->next->next->next->next->next = createnode(6);
+
+    return 0;
+}
