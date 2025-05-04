d3b181f95f7172f6c885975a77bfb2a1aa6e6972
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-05-04 10:47:11 +0530

    Create insertion_at_begg.c

---
+++ LinkedList/insertion_at_begg.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node* START = NULL;
+
+struct Node *createNode(int key)
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = key;
+    newNode->next = NULL;
+    return newNode;
+};
+
+void print()
+{
+    struct Node* ptr = START;
+    while(ptr!=NULL)
+    {
+        printf("%d->",ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
+}
+
+void insert_at_begg(int key)
+{
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    if(temp!=NULL)
+    {
+        temp->data  = key;
+        temp->next = START;
+        START = temp;
+    }
+}
+
+int main()
+{
+    //creating LL: 5->10->1->20->8
+    START = createNode(5);
+    START->next = createNode(10);
+    START->next->next = createNode(1);
+    START->next->next->next = createNode(20);
+    START->next->next->next->next = createNode(8);
+
+    int key;
+    printf("Enter the key: ");
+    scanf("%d",&key);
+    printf("Original list:\n");
+    print();
+
+    insert_at_begg(key);
+
+    printf("Updated list:\n");
+    print();
+
+    return 0;
+}
