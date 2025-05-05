cc5047467a2606d30e0924a7fe8498110da1213a
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-05-05 22:35:49 +0530

    Create swap.c

---
+++ LinkedList/Swap Nodes in Pairs/swap.c
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* swapPairs(struct ListNode* head) {
+    struct ListNode* prev,*curr,*ptr;
+    if(head==NULL || head->next == NULL)
+        return head;
+    ptr = head->next;
+    prev = NULL;
+    curr = head;
+
+    while(curr && curr->next)
+    {
+        struct ListNode* np,*sec;
+        np = curr->next->next;
+        sec = curr->next;
+
+        //swap node
+        sec->next = curr;
+        curr->next = np;
+
+        if(prev)
+            prev->next = sec;
+        
+        prev = curr;
+        curr = np;
+    }
+    return ptr;
+
+}
