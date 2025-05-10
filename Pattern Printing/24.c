eefc63c31923a4f63d9fc46407b777211b6d62de
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-05-10 12:59:12 +0530

    Create 24.c

---
+++ Pattern Printing/24.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int row,star,space,n;
+    printf("Enter number of rows:");
+    scanf("%d",&n);
+
+    for(row=1;row<=n;row++)
+    {
+        for(star=1;star<=(n+1)-row;star++)
+            printf("*");
+
+        for(space=1;space<=2*(row-1);space++)
+            printf(" ");
+
+        for(star=1;star<=(n+1)-row;star++)
+            printf("*");
+        printf("\n");
+    }
+}
