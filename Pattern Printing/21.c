c242ed47db3777778c27370cbe59737499eff467
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-05-10 12:58:32 +0530

    Create 21.c

---
+++ Pattern Printing/21.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d",&n);
+
+    for(int row=1;row<=n;row++)
+    {
+        for(int space=1;space<(n+1)-row;space++)
+            printf(" ");
+        
+        for(int num=1;num<=row;num++)
+            printf("%d ",row);
+        printf("\n");
+    }
+}
