8193bf3c388466772f31b5c3f7d7a23fbd7f198a
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-05-13 10:32:00 +0530

    Create 32.c

---
+++ Pattern Printing/32.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+    int row,col,n;
+    printf("Enter number of rows:");
+    scanf("%d",&n);
+    for(row=1;row<=n;row++)
+    {
+         for(col=1;col<=2*row-1;col++)
+            printf("%d",col);
+        printf("\n");
+    }
+
+    for(row=n;row>=1;row--)
+    {
+        for(col=1;col<=2*row-1;col++)
+            printf("%d",col);
+        printf("\n");
+    }
+}
+
+// Enter number of rows:4
+// 1
+// 123
+// 12345
+// 1234567
+// 1234567
+// 12345
+// 123
+// 1
