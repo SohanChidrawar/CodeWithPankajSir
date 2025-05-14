e5a860afadf8c90613a2e76347c5cf012d9e005e
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-05-14 23:31:02 +0530

    Create min.c

---
+++ Tree/Minimum Depth of Binary Tree/min.c
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int minDepth(struct TreeNode* root) {
+    if(root==NULL)
+        return 0;
+    if(root->left == NULL)
+        return 1+minDepth(root->right);
+
+    if(root->right==NULL)
+        return 1+minDepth(root->left);
+
+    int left = minDepth(root->left);
+    int right = minDepth(root->right);
+
+    int a = (left>right)?right:left;
+
+    return 1+a;
+    
+}
