006b49090817653aedaf33a47caf8d2d19f04d7d
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-05-17 16:10:28 +0530

    Create circularLL_insert_at_begg.c

---
+++ LinkedList/circularLL_insert_at_begg.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *head = NULL;
+
+struct Node *createNode(int key)
+{
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = key;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insert_at_begg(int key)
+{
+    struct Node *node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = key;
+    if(head==NULL)
+    {
+        head = node;
+        node->next = head;
+        return;
+    }
+    struct Node *temp = head;
+    while(temp->next!=head)
+        temp = temp->next;
+
+    node->next = head;
+    head = node;
+    temp->next = node;
+}
+
+void print()
+{
+    if(head==NULL)
+        return;
+
+    struct Node *temp = head;
+    do
+    {
+        printf("%d ",temp->data);
+        temp = temp->next;
+    } while (temp!=head);
+    
+    printf("\n");
+}
+
+int main()
+{
+    struct Node *first = createNode(10);
+    struct Node *second = createNode(20);
+    struct Node *third = createNode(30);
+    struct Node *fourth = createNode(40);
+    struct Node *fifth = createNode(50);
+    head=first;
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = first;
+
+    print();
+    int num;
+    printf("Enter the number to inserted:");
+    scanf("%d",&num);
+    insert_at_begg(num);
+    print();
+
+    return 0;
+
+}
