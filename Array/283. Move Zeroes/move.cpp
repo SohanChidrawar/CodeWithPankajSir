c78d7e9b31c1cee727da58809c02e86e31f5c0aa
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-12-19 14:56:20 +0530

    Create move.cpp

---
+++ Array/283. Move Zeroes/move.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int k = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            {
+                arr[k] = arr[i];
+                k++;
+            }
+        }
+        for(int j=k;j<nums.size();j++)
+            nums[j]=0;
+    }
+};
