ef0ce4f0f338bf42a3068d53af689204113478de
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-12-20 22:23:49 +0530

    Create sort2.cpp

---
+++ Array/Intersection of two sorted array/sort2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    vector<int> intersection(vector<int> &arr1, vector<int> &arr2) {
+        // code here
+        int n = arr1.size();
+        int m = arr2.size();
+        int i=0;
+        int j=0;
+        vector<int> ans;
+        set<int>s ;
+
+        while(i<n && j<m)
+        {
+            if(arr1[i]<arr2[j])
+                i++;
+            else if(arr1[i]>arr2[j])
+                j++;
+            else
+            {
+                s.insert(arr1[i]);
+                i++;
+                j++;
+            }
+        }
+        
+        for(auto it:s)
+            ans.push_back(it);
+        return ans;
+    }
+};
