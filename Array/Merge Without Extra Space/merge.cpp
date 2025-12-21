be67197c05656e8a033fc3d3e54c4c98cd9cbd04
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2025-12-21 13:30:25 +0530

    Update merge.cpp

--- Array/Merge Without Extra Space/merge.cpp
+++ Array/Merge Without Extra Space/merge.cpp
@@ -4,6 +4,8 @@ class Solution {
         // code here
         int n = a.size();
         int m = b.size();
+        // i pointing yo last element of array a and j pointing
+        // to first element of array b and then comparison start
         int i = n-1;
         int j = 0;
         
