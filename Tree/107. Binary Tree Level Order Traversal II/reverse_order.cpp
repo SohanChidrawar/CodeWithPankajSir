7b1ae55e6299dd0d6f91e65e3e57018fc8270b46
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2026-01-15 23:05:36 +0530

    Create reverse_order.cpp

---
+++ Tree/107. Binary Tree Level Order Traversal II/reverse_order.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>>ans;
+        queue<TreeNode *>q;
+
+        if(!root)
+            return ans;
+        
+        q.push(root);
+        while(!q.empty())
+        {
+            int n = q.size();
+            vector<int>level;
+
+            for(int i=0;i<n;i++)
+            {
+                TreeNode *ptr = q.front();
+                level.push_back(ptr->val);
+                if(ptr->left)
+                    q.push(ptr->left);
+                if(ptr->right)
+                    q.push(ptr->right);
+                q.pop();
+            }
+            ans.push_back(level);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
