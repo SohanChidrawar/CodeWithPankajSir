a807c5020aebc0247bbb4f128960d5a7a5356849
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2026-02-06 23:33:37 +0530

    Create one.cpp

---
+++ Array/66. Plus One/one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int>result;
+        int n = digits.size();
+        int carry = 1;
+        for(int i=n-1;i>=0;i--)
+        {
+            int sum = digits[i] + carry;
+            digits[i] = sum % 10;
+            carry = sum / 10;
+        }
+
+        if(carry)
+            digits.insert(digits.begin(),carry);
+        
+        return digits;
+    }
+};
