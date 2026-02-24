b21314ace47291787087dc3192fa40e04ce7dddd
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2026-02-24 21:17:44 +0530

    Create dfs.cpp

---
+++ Graphs/DFS of Graph/dfs.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+  
+    void dfsrec(vector<vector<int>>& adj, vector<bool>& visited,
+    int s, vector<int> &res)
+    {
+        visited[s] = true;
+        
+        res.push_back(s);
+        
+        for(int i : adj[s])
+        {
+            if(visited[i] == false)
+            {
+                dfsrec(adj,visited,i,res);
+            }
+        }
+    }
+  
+    vector<int> dfs(vector<vector<int>>& adj) {
+        // Code here
+        vector<bool> visited(adj.size(), false);
+        vector<int> res;
+        dfsrec(adj,visited,0,res);
+        
+        return res;
+        
+    }
+};
