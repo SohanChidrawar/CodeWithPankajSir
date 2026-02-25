a9ee461a583cb709eb4f7d05f3613f13e5cd7d68
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2026-02-25 20:28:11 +0530

    Create subarray.cpp

---
+++ Array/Longest Subarray with Majority Greater than K/subarray.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    int longestSubarray(vector<int> &arr, int k) {
+        
+        int n = arr.size();
+        unordered_map<int,int>mp;
+        int ans=0,sum=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>k)
+                sum++;
+            else
+                sum--;
+                
+            if(sum>0)
+                ans = i+1;
+            else
+            {
+                if(mp.find(sum-1) != mp.end())
+                    ans = max(ans,i-mp[sum-1]);
+            }
+            
+            //store first occurence of each prefix
+            if(mp.find(sum) == mp.end())
+                mp[sum] = i;
+        }
+        
+        return ans;
+    }
+};
