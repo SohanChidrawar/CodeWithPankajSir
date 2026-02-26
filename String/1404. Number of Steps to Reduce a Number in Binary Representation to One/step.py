c15060171f1f3867c3e157414b451d893768b55c
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2026-02-26 19:38:15 +0530

    Create step.py

---
+++ String/1404. Number of Steps to Reduce a Number in Binary Representation to One/step.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numSteps(self, s: str) -> int:
+        num = int(s,2)
+        step = 0
+
+        while num!=1:
+            if num%2==0:
+                num //=2
+            else:
+                num+=1
+            step += 1
+
+        return step
+        
