4ae769de8de55c1a0fa2f39d467b66a5c921f8fd
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2026-02-28 19:32:41 +0530

    Create binary.cpp

---
+++ Accustomed/1680. Concatenation of Consecutive Binary Numbers/binary.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        const int mod = 1e9+7;
+        long long res = 0;
+        int bits = 0;
+
+        for(int i=1;i<=n;i++)
+        {
+            if((i & (i-1)) == 0)
+                bits++;
+            
+            res = ((res<<bits) | i) % mod;
+
+        }
+
+        return res;
+
+    }
+};
