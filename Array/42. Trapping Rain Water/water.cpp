1d7ac7c7808ad0b1c222a269c113c8c45b0890a7
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2026-03-02 19:09:15 +0530

    Update water.cpp

--- Array/42. Trapping Rain Water/water.cpp
+++ Array/42. Trapping Rain Water/water.cpp
@@ -20,6 +20,30 @@ public:
 
         return count;
     }
-
-    
 };
+
+// class Solution {
+//   public:
+//     int maxWater(vector<int> &arr) {
+//         // code here
+//         int n = arr.size();
+//         int water = 0;
+//         vector<int>left(n,0);
+//         vector<int>right(n,0);
+        
+//         left[0] = arr[0];
+//         for(int i=1;i<n;i++)
+//             left[i] = max(left[i-1],arr[i]);
+        
+//         right[n-1] = arr[n-1];
+//         for(int i=n-2;i>=0;i--)
+//             right[i] = max(right[i+1],arr[i]);
+            
+        
+//         for(int i=0;i<n;i++)
+//             water += min(left[i],right[i]) - arr[i];
+            
+        
+//         return water;
+//     }
+// };
