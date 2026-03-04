6ace728cd1640a958c72d57b5a06a951495e8b5b
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2026-03-04 21:01:20 +0530

    Create matrix.cpp

---
+++ Array/1582. Special Positions in a Binary Matrix/matrix.cpp
@@ -0,0 +1,50 @@
+/*
+Approach
+Create two arrays:
+row[i] → count of 1s in row i
+col[j] → count of 1s in column j
+First pass:
+Traverse matrix
+Count 1s for every row and column
+Second pass:
+If mat[i][j] == 1
+AND row[i] == 1
+AND col[j] == 1
+Increase answer
+*/
+
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        vector<int>row(m,0);
+        vector<int>col(n,0);
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(mat[i][j] == 1)
+                {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(mat[i][j]==1 && row[i]==1 && col[j]==1)
+                    ans++;
+            }
+        }
+
+        return ans;
+    }
+};
