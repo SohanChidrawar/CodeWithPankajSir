9aecd7c4bf3b1a0582749275b2fed63ebe819cd5
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2026-03-06 23:12:13 +0530

    Create remove.py

---
+++ String/Remove Duplicates/remove.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+class Solution:
+	def removeDups(self, str):
+		# code here
+		seen = set()
+		result = []
+		
+		for char in str:
+		    if char not in seen:
+		        seen.add(char)
+		        result.append(char)
+		        
+		return "".join(result)
