b457b684c1741667508f0f4459e4db047781caaf
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2026-03-31 14:56:03 +0530

    Implement memoization for stock profit calculation
    Added memoization approach to optimize profit calculation.

--- DP/714. Best Time to Buy and Sell Stock with Transaction Fee/memo.cpp
+++ DP/714. Best Time to Buy and Sell Stock with Transaction Fee/memo.cpp
@@ -1,3 +1,9 @@
+/*
+The above approach can be optimized using memoization. The idea is to create a 2d array memo[][] of order n*2, where element memo[i][0] stores the 
+maximum profit from day i to n-1 when no previous stock is pending and memo[i][1] stores maximum profit from day i to n-1 when there is previous stock remaining. 
+For each recursive call, check if the sub-array is already computed, if so return the stored value else proceed as in above approach.
+*/
+
 class Solution {
 public:
 
