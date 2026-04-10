6c4f0a105fe0a7cb9635a9ec76096627030bb2c9
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2026-04-10 10:10:07 +0530

    Create merge.cpp

---
+++ Array/349. Intersection of Two Arrays/merge.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    vector<int> intersection(vector<int>& a, vector<int>& b) {
+        // code here
+        int m = a.size();
+        int n = b.size();
+        vector<int>result;
+        int i=0,j=0;
+        
+        while(i<m && j<n)
+        {
+            // skip duplicate element from first array
+            if(i>0 && a[i-1] == a[i])
+            {
+                i++;
+                continue;
+            }
+            
+            // skip smaller element
+            if(a[i]<b[j])
+                i++;
+            else if(a[i]>b[j])
+                j++;
+            else
+            {
+                result.push_back(a[i]);
+                i++;
+                j++;
+            }
+        }
+        return result;
+    }
+};
