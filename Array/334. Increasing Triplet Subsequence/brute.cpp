fff66203825d66a7e8a1ab56d2612d37b21a2796
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2026-04-10 20:25:45 +0530

    Create brute.cpp

---
+++ Array/334. Increasing Triplet Subsequence/brute.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n = nums.size();
+        if(n<3)
+            return false;
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[i] < nums[j])
+                for(int k=j+1;k<n;k++)
+                {
+                    if(nums[j] < nums[k])
+                        return true;
+                }
+            }
+        }
+        return false
+    }
+};
